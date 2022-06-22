23874e20e232c2e7e01ee99dbe0d728418d225ef
PolinaChe <cambojha25@gmail.com>
2022-06-23 01:40:54 +0300

    999

--- java_books/src/lesson1/Exercise1.java
+++ java_books/src/lesson1/Exercise1.java
@@ -1,3 +1,5 @@
+package lesson1;
+
 public class Exercise1 {
 
     public static void main(String[] args) {
--- java_books/src/lesson1/Shuffle1.java
+++ java_books/src/lesson1/Shuffle1.java
@@ -1,3 +1,5 @@
+package lesson1;
+
 public class Shuffle1 {
     public static void main(String[] args) {
         int x = 3;
